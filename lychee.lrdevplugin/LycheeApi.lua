5883ee9510cd30d87e4a056d59216c21ee280025
Chris Searle <chris@chrissearle.org>
2020-09-17 20:29:48 +0200

    Add test button that actually tests login

--- lychee.lrdevplugin/LycheeApi.lua
+++ lychee.lrdevplugin/LycheeApi.lua
@@ -1,52 +1,103 @@
-local LrErrors = import 'LrErrors'
+local LrErrors = import "LrErrors"
+local LrDialogs = import "LrDialogs"
 local LrHttp = import "LrHttp"
+local logger = import "LrLogger"("Lychee")
 
-local prefs = import 'LrPrefs'.prefsForPlugin()
+logger:enable("logfile")
 
-local logger = import 'LrLogger'( 'Lychee' )
+local prefs = import "LrPrefs".prefsForPlugin()
 
-LycheeApi = {}
+LycheeAPI = {}
 
+local function getCookies(data)
+	local cookies = {}
 
-function LycheeApi.getConfig()
-
-	local url, api_key, username, password = prefs.lychee_site_url, prefs.lychee_api_key, prefs.lychee_username, prefs.lychee_password
-	
-	if not(
-		type( url ) == 'string' and #url >= 10 and
-		type( api_key ) == 'string' and #api_key >= 1 and
-		type( username ) == 'string' and #username >= 1 and
-		type( password ) == 'string' and #password >= 1
-    ) then do
-        LrErrors.throwUserError( "You need to enter the configuration via Plugin Manager" )
+	for _, v in pairs(data) do
+		if (type(v) == "table") then
+			if (v.field == "Set-Cookie") then
+				cookies[#cookies + 1] = v.value
+			end
+		end
 	end
-	
+
+	return cookies
+end
+
+local function initHeaders(headers)
+	local cookies = LycheeAPI.init()
+
+	for _, cookie in pairs(cookies) do
+		c = LrHttp.parseCookie(cookie)
+
+		if c["lychee_session"] ~= nil then
+			headers[#headers + 1] = {
+				field = "Cookie",
+				value = "lychee_session=" .. c["lychee_session"]
+			}
+		end
+	end
+
+	return headers
+end
+
+function LycheeAPI.getConfig()
+	local url, api_key, username, password =
+		prefs.lychee_site_url,
+		prefs.lychee_api_key,
+		prefs.lychee_username,
+		prefs.lychee_password
+
+	if
+		not (type(url) == "string" and #url >= 10 and type(api_key) == "string" and #api_key >= 1 and
+			type(username) == "string" and
+			#username >= 1 and
+			type(password) == "string" and
+			#password >= 1)
+	 then
+		logger.info("Did not find enough config")
+
+		LrErrors.throwUserError("You need to enter the configuration via Plugin Manager")
+	end
+
 	return url, api_key, username, password
 end
 
+function LycheeAPI.init()
+	local url, api_key, username, password = LycheeAPI.getConfig()
 
-function LycheeApi.login()
-    local url, api_key, username, password = getConfig()
+	local init_url = url .. "/api/Session::init"
 
-    local loginResponse = LrHttp.post(
-		url .. '/api/Session::login',
-		'user=' .. username .. '&password=' .. password,
-		{
-			{
-				'field' = 'Content-Type',
-				'value' = 'x-www-form-urlencoded'
-			},
-			{
-				'field' = 'Authorization',
-				'value' = api_key
-			}
-		},
-		'POST'
-	)
+	logger:info("Calling init on " .. init_url)
 
-	if not(loginResponse == 'true') then do
-		 LrErrors.throwUserError( "login failed - please check the configuration via Plugin Manager" )
-	end
+	local headers = {
+		{field = "Authorization", value = api_key}
+	}
+
+	local _, data = LrHttp.post(init_url, "", headers, "POST")
+
+	return getCookies(data)
 end
 
-function 
+function LycheeAPI.login()
+	local url, api_key, username, password = LycheeAPI.getConfig()
+
+	local login_url = url .. "/api/Session::login"
+
+	logger:info("Calling login on " .. login_url)
+
+	local headers =
+		initHeaders(
+		{
+			{field = "Authorization", value = api_key},
+			{field = "Content-Type", value = "application/x-www-form-urlencoded"}
+		}
+	)
+
+	local loginResponse, data = LrHttp.post(login_url, "user=" .. username .. "&password=" .. password, headers, "POST")
+
+	if (loginResponse == "true") then
+		LrDialogs.message("Login succeeded")
+	else
+		LrErrors.throwUserError("Login failed - please check the configuration via Plugin Manager")
+	end
+end
--- lychee.lrdevplugin/PluginInfoProvider.lua
+++ lychee.lrdevplugin/PluginInfoProvider.lua
@@ -1,77 +1,88 @@
-local LrView = import 'LrView'
+local LrView = import "LrView"
 local bind = LrView.bind
-local prefs = import 'LrPrefs'.prefsForPlugin()
+local prefs = import "LrPrefs".prefsForPlugin()
 
-local function sectionsForTopOfDialog( f, _ )
+require "LycheeAPI"
+
+local function sectionsForTopOfDialog(f, _)
 	return {
-			{
-				title = "Lychee Server and Login",
-				bind_to_object = prefs,
-				f:row {
-					spacing = f:control_spacing(),
-
-					f:static_text {
-						title = "Lychee Site URL",
-						width_in_chars = 20,
-					},
-					f:edit_field {
-						value = bind 'lychee_site_url',
-						fill_horizonal = 1,
-						width_in_chars = 35,
-					}
+		{
+			title = "Lychee Server and Login",
+			bind_to_object = prefs,
+			f:row {
+				spacing = f:control_spacing(),
+				f:static_text {
+					title = "Lychee Site URL",
+					width_in_chars = 20
 				},
-				f:row {
-					spacing = f:control_spacing(),
-
-					f:static_text {
-						title = "Lychee API Key",
-						width_in_chars = 20,
-					},
-					f:edit_field {
-						value = bind 'lychee_api_key',
-						fill_horizonal = 1,
-						width_in_chars = 35,
-					}
-				},
-				f:row {
-					spacing = f:control_spacing(),
-					f:static_text {
-						title = "To set an API key in Lychee - Settings > Advanced > api_key",
-						fill_horizontal = 1,
-						size = 'small',
-					},
-				},
-				f:row {
-					spacing = f:control_spacing(),
-
-					f:static_text {
-						title = "Username",
-						width_in_chars = 20,
-					},
-					f:edit_field {
-						value = bind 'lychee_username',
-						fill_horizonal = 1,
-						width_in_chars = 35,
-					}
-				},
-				f:row {
-					spacing = f:control_spacing(),
-
-					f:static_text {
-						title = "Password",
-						width_in_chars = 20,
-					},
-					f:password_field {
-						value = bind 'lychee_password',
-						fill_horizonal = 1,
-						width_in_chars = 35,
-					}
-				},
+				f:edit_field {
+					value = bind "lychee_site_url",
+					fill_horizonal = 1,
+					width_in_chars = 35
+				}
 			},
-	
+			f:row {
+				spacing = f:control_spacing(),
+				f:static_text {
+					title = "Lychee API Key",
+					width_in_chars = 20
+				},
+				f:edit_field {
+					value = bind "lychee_api_key",
+					fill_horizonal = 1,
+					width_in_chars = 35
+				}
+			},
+			f:row {
+				spacing = f:control_spacing(),
+				f:static_text {
+					title = "To set an API key in Lychee - Settings > Advanced > api_key",
+					fill_horizontal = 1,
+					size = "small"
+				}
+			},
+			f:row {
+				spacing = f:control_spacing(),
+				f:static_text {
+					title = "Username",
+					width_in_chars = 20
+				},
+				f:edit_field {
+					value = bind "lychee_username",
+					fill_horizonal = 1,
+					width_in_chars = 35
+				}
+			},
+			f:row {
+				spacing = f:control_spacing(),
+				f:static_text {
+					title = "Password",
+					width_in_chars = 20
+				},
+				f:password_field {
+					value = bind "lychee_password",
+					fill_horizonal = 1,
+					width_in_chars = 35
+				}
+			},
+			f:row {
+				spacing = f:control_spacing(),
+				f:push_button {
+					enabled = true,
+					title = "Test",
+					action = function(button)
+						import "LrTasks".startAsyncTask(
+							function()
+								LycheeAPI.login()
+							end
+						)
+					end
+				}
+			}
 		}
+	}
 end
 
 return {
-	sectionsForTopOfDialog = sectionsForTopOfDialog,
+	sectionsForTopOfDialog = sectionsForTopOfDialog
 }
